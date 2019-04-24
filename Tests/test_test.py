6390e8cffcda840e9943ab81a43649b582549e7c
Lukasz Kolodzinski <luk.kolodzinski@gmail.com>
2019-04-24 18:42:30 +0200

    first commit

---
+++ Tests/test_test.py
@@ -0,0 +1,15 @@
+#REFERENCE TEST FILE - Will be deleted in future
+
+from selenium import webdriver
+from time import sleep
+
+driver = webdriver.Firefox()
+driver.set_page_load_timeout(20)
+driver.get("http://the-internet.herokuapp.com/")
+sleep(2)
+print(driver.title)
+driver.maximize_window()
+driver.get_screenshot_as_file("/home/lucas/SELENIUM/t-i.webapp.selenium/Screenshots/home_page.png")
+driver.implicitly_wait(10)
+driver.close()
+driver.quit()
