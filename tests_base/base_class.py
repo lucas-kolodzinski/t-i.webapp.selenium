fdedf0a937b2806c7fe3cfc9fea8c8191b4a312d
Lukasz Kolodzinski <abrakadabra1922@gmail.com>
2019-05-22 22:44:40 +0200

    base class implementation

---
+++ tests_base/base_class.py
@@ -0,0 +1,22 @@
+import unittest
+import datetime
+from selenium import webdriver
+
+class EnvironmentSetup(unittest.TestCase):
+
+    def setUp(self):
+        self.driver = webdriver.Firefox()
+        self.time = str(datetime.datetime.now())
+        print("Test started at: {}".format(self.time))
+        self.driver.get("https://the-internet.herokuapp.com/")
+        self.driver.implicitly_wait(15)
+        self.driver.maximize_window()
+
+    #temporary test to verify base class work
+    def test_abc(self):
+        print("Page title: {}".format(self.driver.title))
+
+    def tearDown(self):
+        print("Execution finished at: {}".format(self.time))
+        self.driver.close()
+        self.driver.quit()
