8279337c26755be06688b97f3ded65cbb3982a3a
Lukasz Kolodzinski <abrakadabra1922@gmail.com>
2019-07-09 19:19:11 +0200

    Base class change: commented print time, url, title

--- tests_base/base_class.py
+++ tests_base/base_class.py
@@ -7,13 +7,13 @@ class EnvironmentSetup(unittest.TestCase):
 
     def setUp(self):
         self.driver = webdriver.Firefox()
-        self.time = str(datetime.datetime.now())
-        print("Test started at: {}".format(self.time))
+        #self.time = str(datetime.datetime.now())
+        #print("Test started at: {}".format(self.time))
         self.driver.get("https://the-internet.herokuapp.com/")
         self.driver.implicitly_wait(15)
         self.driver.maximize_window()
-        print("Page web address is: {}".format(self.driver.current_url))
-        print("Page title is: {}".format(self.driver.title))
+        #print("Page web address is: {}".format(self.driver.current_url))
+        #print("Page title is: {}".format(self.driver.title))
 
     def tearDown(self):
         print("Execution finished at: {}".format(self.time))
